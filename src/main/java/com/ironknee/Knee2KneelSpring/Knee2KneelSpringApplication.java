fec8b980b1af0e817e95d0a73654708102e2fe66
SHKim55 <5061ksh@naver.com>
2024-09-30 00:24:45 +0900

    initial commit

---
+++ src/main/java/com/ironknee/Knee2KneelSpring/Knee2KneelSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ironknee.Knee2KneelSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knee2KneelSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Knee2KneelSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironknee/Knee2KneelSpring/Knee2KneelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironknee.Knee2KneelSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knee2KneelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
