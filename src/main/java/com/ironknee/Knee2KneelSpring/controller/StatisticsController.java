28eaa492a1c4636d55add62402cb82acbe030d8a
SHKim55 <5061ksh@naver.com>
2024-10-14 00:28:47 +0900

    refactor : change API endpoint address

--- src/main/java/com/ironknee/Knee2KneelSpring/controller/StatisticsController.java
+++ src/main/java/com/ironknee/Knee2KneelSpring/controller/StatisticsController.java
@@ -1,9 +1,11 @@
 package com.ironknee.Knee2KneelSpring.controller;
 
 import com.ironknee.Knee2KneelSpring.dto.ResponseObject;
+import com.ironknee.Knee2KneelSpring.dto.statistics.GameResultDTO;
 import com.ironknee.Knee2KneelSpring.dto.statistics.StatisticsDTO;
 import com.ironknee.Knee2KneelSpring.service.statistics.StatisticsService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +20,18 @@ public class StatisticsController {
         this.statisticsService = statisticsService;
     }
 
-    @GetMapping("/user")
+    @GetMapping("/read/user")
     public ResponseObject<StatisticsDTO> getStatisticsByUser(@RequestBody UUID userId) {
         return statisticsService.getUserStatisticsByUser(userId);
     }
 
-    @GetMapping("/id")
+    @GetMapping("/read/id")
     public ResponseObject<StatisticsDTO> getStatisticsById(@RequestBody Long statisticsId) {
         return statisticsService.getUserStatisticsById(statisticsId);
     }
+
+//    @PostMapping("/update")    // 게임 종료 시 통계치 업데이트
+//    public ResponseObject<Boolean> updateStatistics(@RequestBody GameResultDTO gameResultDTO) {
+//        return statisticsService.updateStatistics(gameResultDTO);
+//    }
 }
