921ef4d9811acd56c1779afd6d44e29dd5a77b02
SHKim55 <5061ksh@naver.com>
2024-11-06 19:13:37 +0900

    feat : add player status fields and brand-new role granting method

--- src/main/java/com/ironknee/Knee2KneelSpring/service/player/Player.java
+++ src/main/java/com/ironknee/Knee2KneelSpring/service/player/Player.java
@@ -1,7 +1,10 @@
 package com.ironknee.Knee2KneelSpring.service.player;
 
+import com.ironknee.Knee2KneelSpring.service.game.Game;
+import com.ironknee.Knee2KneelSpring.service.user.UserRank;
 import lombok.*;
 
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
@@ -13,12 +16,16 @@ import java.util.UUID;
 public class Player {
     private UUID userId;
     private String userEmail;
+    private String userNickname;
+    private Long level;
+    private UserRank userRank;
+    private Long rankPoint;
     private PlayerRole playerRole;
     private Boolean isAdmin;
     private Boolean isReady;
 
 //    private List<Skill> skillList;
-//    private PlayerConcept playerConcept;
+//    private PlayerCharacter playerCharacter;
 
     public static PlayerRole getRandomRole() {
         PlayerRole[] playerRoles = PlayerRole.values();
@@ -27,5 +34,23 @@ public class Player {
         int randomIndex = random.nextInt(playerRoles.length - 1) + 1;   // 교수(index 0)는 랜덤 역할 생성에서 제외
 
         return playerRoles[randomIndex];
+    }
+
+    public static PlayerRole grantPlayerRole(final Game game) {
+        boolean hasProfessor = false;
+        long countStudent = 0L, maxStudent = 5L;
+
+        List<Player> playerList = game.getPlayerList();
+
+        for(Player player : playerList) {
+            if(player.getPlayerRole() == PlayerRole.professor)
+                hasProfessor = true;
+            else if(player.getPlayerRole() == PlayerRole.student)
+                countStudent++;
+        }
+
+        if(!hasProfessor) return PlayerRole.professor;
+        if(countStudent <= maxStudent - 1) return PlayerRole.student;
+        else return PlayerRole.assistant;
     }
 }
