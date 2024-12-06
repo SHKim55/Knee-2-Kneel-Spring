3a8e7100130f01c464c43d74d73721c708a69670
SHKim55 <5061ksh@naver.com>
2024-12-06 15:00:53 +0900

    feat: add DTO with player id list (UUID) for checking active users

--- src/main/java/com/ironknee/Knee2KneelSpring/dto/statistics/GameResultDTO.java
+++ src/main/java/com/ironknee/Knee2KneelSpring/dto/statistics/GameResultDTO.java
@@ -1,4 +1,13 @@
 package com.ironknee.Knee2KneelSpring.dto.statistics;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
 public class GameResultDTO {
+    private List<UUID> playerIdList;
 }
